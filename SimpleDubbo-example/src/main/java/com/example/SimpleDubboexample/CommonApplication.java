465178d275ca069225590a6d482cb13143589035
Carver_007 <k995211314@163.com>
2019-05-12 15:16:42 +0800

    Update CommonApplication.java

--- SimpleDubbo-example/src/main/java/com/example/SimpleDubboexample/CommonApplication.java
+++ SimpleDubbo-example/src/main/java/com/example/SimpleDubboexample/CommonApplication.java
@@ -1,13 +1,9 @@
 package com.example.SimpleDubboexample;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class CommonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CommonApplication.class, args);
+		
 	}
 
 }
