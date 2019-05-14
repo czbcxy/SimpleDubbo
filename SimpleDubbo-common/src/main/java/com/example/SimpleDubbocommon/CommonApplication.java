89da8399d90ecd4faac5b148dbb293e4ba27bfb1
jiakai <2537254734@qq.com>
2019-05-14 23:36:36 +0800

    添加slf4j与log4j2日志坐标以及sl4j2.xml

--- SimpleDubbo-common/src/main/java/com/example/SimpleDubbocommon/CommonApplication.java
+++ SimpleDubbo-common/src/main/java/com/example/SimpleDubbocommon/CommonApplication.java
@@ -1,8 +1,17 @@
 package com.example.SimpleDubbocommon;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CommonApplication {
 
 	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(CommonApplication.class);
+		logger.info("hello info");
+		logger.error("hello error");
+		logger.warn("hello warn");
+		logger.debug("hello debug");
+		logger.trace("hello trace");
 	}
 
 }
